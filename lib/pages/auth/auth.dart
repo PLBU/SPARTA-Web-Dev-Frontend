6825a6300e7a3723fe8016baea7015d12145bbd7
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-06-02 11:00:36 +0700

    Example auth page

---
+++ lib/pages/auth/auth.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("This is auth page"),
+    );
+  }
+}
