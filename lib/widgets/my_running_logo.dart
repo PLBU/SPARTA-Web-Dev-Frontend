da19652fdc7d6531c2c3cafefe407a4863afc723
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-06-04 15:39:29 +0700

    add responsive and padding

--- lib/widgets/my_running_logo.dart
+++ lib/widgets/my_running_logo.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:sparta/utils/ui_utils.dart';
 
 class MyRunningLogo extends StatelessWidget {
   const MyRunningLogo();
 
   @override
   Widget build(BuildContext context) {
+    DeviceType deviceType = UIUtils.getDeviceType(context);
+    double size = deviceType == DeviceType.mobile ? 30 : 50;
+
     return Container(
       width: double.infinity,
+      padding: const EdgeInsets.only(top: 7, bottom: 7),
       color: Colors.black,
       child: Image.asset(
         'images/PatternSparta.png',
-        height: 80,
-        width: 80,
+        height: size,
+        width: size,
         repeat: ImageRepeat.repeatX,
       ),
     );
