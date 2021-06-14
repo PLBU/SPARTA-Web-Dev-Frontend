ecdda3328ebb6c542e0221048adb3affe6af0eb0
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-06-14 20:02:57 +0700

    modify search_bar

--- lib/pages/scoreboard/views/search_bar.dart
+++ lib/pages/scoreboard/views/search_bar.dart
@@ -9,46 +9,39 @@ class MySearchBar extends StatelessWidget {
     DeviceType deviceType = UIUtils.getDeviceType(context);
 
     double respWidth = (deviceType == DeviceType.mobile)
-        ? 350
+        ? 355
         : (deviceType == DeviceType.tablet)
-            ? 600
-            : 1000;
+            ? 605
+            : 1010;
 
     double respFont = (deviceType == DeviceType.mobile)
         ? 10
         : (deviceType == DeviceType.tablet)
             ? 13
-            : 18;
+            : 16;
 
     return Container(
       padding: (deviceType == DeviceType.mobile)
-          ? EdgeInsets.only(top: 2, bottom: 2, left: 5)
-          : EdgeInsets.only(top: 5, bottom: 5, left: 5),
+          ? EdgeInsets.symmetric(vertical: 2, horizontal: 5)
+          : EdgeInsets.symmetric(vertical: 5, horizontal: 5),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black),
         borderRadius: BorderRadius.circular(10),
         color: Colors.white,
       ),
-      height: (deviceType == DeviceType.mobile) ? 30 : 50,
+      height: (deviceType == DeviceType.mobile) ? 50 : 80,
       width: respWidth,
-      child: Row(
+      child: Column(
         children: [
           Container(
-            width: respWidth * 0.5,
+            width: respWidth,
+            height: (deviceType == DeviceType.mobile) ? 30 : 50,
             child: searchBar(context, respFont * 2),
           ),
-          SizedBox(width: respFont),
-          Container(
-            height: 50,
-            width: respWidth * 0.43,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Dapat mencari dengan jurusan=IF; nama=test;",
-                  style: TextStyle(fontFamily: "Roboto", fontSize: respFont),
-                ),
-              ],
+          Expanded(
+            child: Text(
+              "Dapat mencari dengan jurusan=IF; nama=test;",
+              style: TextStyle(fontFamily: "Roboto", fontSize: respFont),
             ),
           )
         ],
