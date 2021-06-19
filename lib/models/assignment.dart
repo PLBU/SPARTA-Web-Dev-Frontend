43a4f1c5e4b86133370c41bc8d2ae207aa69ac17
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-06-19 23:50:34 +0700

    Added Assignment model

--- lib/models/assignment.dart
+++ lib/models/assignment.dart
@@ -3,17 +3,17 @@ class Assignment {
   final String name;
   final String spek;
   final DateTime deadline;
-  final List<String> arrayOfUsers;
+  final List<String> arrayofUsers;
 
-  const Assignment({this.id, this.name, this.spek, this.deadline, this.arrayOfUsers});
+  const Assignment({this.id, this.name, this.spek, this.deadline, this.arrayofUsers});
 
   factory Assignment.fromJson(dynamic json) {
     return Assignment(
       id: json['_id'] as String,
       name: json['name'] as String,
       spek: json['spek'] as String,
-      deadline: json['deadline'] as DateTime,
-      arrayOfUsers: new List<String>.from(json['arrayOfUsers'])
+      deadline: DateTime.parse(json['deadline']),
+      arrayofUsers: (json['arrayofUsers'] != null) ? List<String>.from(json['arrayofUsers']) : []
     );
   }
 }
