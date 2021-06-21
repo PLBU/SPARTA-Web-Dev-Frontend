b1d1600f02df3b8286164e28a72b990b5d994f49
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-06-21 23:31:27 +0700

    fetch assignments

---
+++ lib/pages/upload/services/index.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:sparta/utils/network_util.dart';
+import 'package:sparta/models/assignment.dart';
+import 'package:sparta/models/submission.dart';
+
+Future<List<Assignment>> fetchAssignments(String token) async {
+  final headers = {
+    'Authorization': 'Bearer $token',
+  };
+  final uri = NetworkUtil.getApiUrl(
+    route: 'assignments',
+  );
+  final response = await http.get(uri, headers: headers);
+
+  var list = jsonDecode(response.body) as List;
+  List<Assignment> assignments =
+      list.map((obj) => Assignment.fromJson(obj)).toList();
+
+  return assignments;
+}
