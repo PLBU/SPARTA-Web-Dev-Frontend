1eb85df9cc66b6b2e101d5c3b8c61fe564821fef
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-06-25 23:14:24 +0700

    support page done

--- lib/pages/support/services/index.dart
+++ lib/pages/support/services/index.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:sparta/models/support.dart';
 import 'package:sparta/utils/network_util.dart';
+import 'package:sparta/models/user.dart';
+import 'package:sparta/pages/support/views/support_card.dart';
 
-Future<List<Support>> fetchSupports(String id, String jwt, bool isPengirim) async {
+Future<List<Support>> fetchSupports(
+    String id, String jwt, bool isPengirim) async {
   final headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer $jwt'
@@ -23,4 +27,42 @@ Future<List<Support>> fetchSupports(String id, String jwt, bool isPengirim) asyn
   } else {
     return null;
   }
+}
+
+Future<User> fetchUser(var id) async {
+  final uri = NetworkUtil.getApiUrl(
+    route: 'users/custom',
+    queryParams: {
+      'id': id,
+      'simple': '1',
+    },
+  );
+  final response = await http.get(uri);
+
+  if (response.statusCode==200){
+    User suppUser = User.fromJson(jsonDecode(response.body));
+
+    return suppUser;
+  }
+  return null;
+}
+
+Future<List<dynamic>> fetchLists(String id, String jwt, bool isPengirim) async {
+  var supports = await fetchSupports(id, jwt, isPengirim);
+
+  if (supports==null) return null;
+
+  List<dynamic> resArr = [];
+  for (var supp in supports) {
+    User user = await fetchUser(isPengirim ? supp.penerima : supp.pengirim);
+    if (user==null) continue;
+    
+    resArr.add(SupportCard(
+      suppInfo: supp.text,
+      isPengirim: isPengirim,
+      user: user,
+    ));
+  }
+
+  return resArr;
 }
--- lib/pages/support/views/support_card.dart
+++ lib/pages/support/views/support_card.dart
@@ -3,29 +3,24 @@ import 'package:sparta/widgets/my_button.dart';
 import 'package:sparta/utils/ui_utils.dart';
 import 'package:sparta/models/support.dart';
 import 'package:sparta/models/user.dart';
-import 'package:sparta/pages/support/services/fetchUser.dart';
-
+import 'package:sparta/pages/send_support/support_dialog.dart';
+import 'package:sparta/pages/support/services/index.dart';
 
 class SupportCard extends StatelessWidget {
   const SupportCard({
     this.suppInfo,
     this.isPengirim,
+    this.user,
   });
 
-  final Support suppInfo;
+  final String suppInfo;
   final bool isPengirim;
+  final User user;
 
   @override
   Widget build(BuildContext context) {
-    Future<User> suppResp;
-    User user;
-
-    suppResp = fetchUser( (isPengirim) ? suppInfo.pengirim : suppInfo.penerima);
-    suppResp.then((User val){
-      user = val;
-    });
-
     DeviceType deviceType = UIUtils.getDeviceType(context);
+
     double space = (deviceType == DeviceType.mobile)
         ? 10
         : (deviceType == DeviceType.tablet)
@@ -42,70 +37,71 @@ class SupportCard extends StatelessWidget {
             ? 30
             : 40;
 
-    return FutureBuilder(
-      future: suppResp,
-      builder: (context, snapshot){
-        if (snapshot.hasData){
-          return Wrap(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(respPad),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 0.0,
-                      offset: Offset(0.0, 2.0),
-                    ),
-                  ],
-                ),
-
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(user.nim + " " + user.namaLengkap, style: TextStyle(
-                          fontSize: respFont * 1.7,
-                          fontWeight: FontWeight.bold,
-                        )),
-                        if (!(deviceType == DeviceType.mobile)) MyButton(text: 'SUPPORT', buttonType: ButtonType.black,),
-                      ],
-                    ),
-                    SizedBox(height: space),
-                    Text(suppInfo.text, style: TextStyle(
-                      fontSize: respFont * 2,
-                    )),
-                    SizedBox(height: space),
-                    if (deviceType == DeviceType.mobile) SizedBox(height: space),
-                    if (deviceType == DeviceType.mobile) Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        SizedBox(width: space),
-                        MyButton(text: 'SUPPORT', buttonType: ButtonType.black,),
-                      ],
-                    )
-                    
-                  ],
-                ),
+    return Wrap(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(respPad),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.black,
+              width: 1.0,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 0.0,
+                offset: Offset(0.0, 2.0),
               ),
             ],
-          );
-        }
-
-        return Center(
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
           ),
-        );
-      }
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(this.user.nim + " " + this.user.namaLengkap,
+                      style: TextStyle(
+                        fontSize: respFont * 1.7,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  if (!(deviceType == DeviceType.mobile))
+                    MyButton(
+                      text: 'SUPPORT',
+                      buttonType: ButtonType.black,
+                      handler: (){
+                        showSupportDialog(context, this.user.namaLengkap, this.user.id);
+                      },
+                    ),
+                ],
+              ),
+              SizedBox(height: space),
+              Text(suppInfo,
+                  style: TextStyle(
+                    fontSize: respFont * 2,
+                  )),
+              SizedBox(height: space),
+              if (deviceType == DeviceType.mobile) SizedBox(height: space),
+              if (deviceType == DeviceType.mobile)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(width: space),
+                    MyButton(
+                      text: 'SUPPORT',
+                      buttonType: ButtonType.black,
+                      handler: (){
+                        showSupportDialog(context, this.user.namaLengkap, this.user.id);
+                      },
+                    ),
+                  ],
+                )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
