74f3a27850c37771db4b2129a682edf6aab597dd
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-07-01 21:11:37 +0700

    Flutter downgrade to 2.2.1

--- flutter-netlify-build.sh
+++ flutter-netlify-build.sh
@@ -2,5 +2,7 @@ git clone https://github.com/flutter/flutter.git
 FLUTTER=flutter/bin/flutter
 FLUTTER_CHANNEL=stable
 $FLUTTER channel $FLUTTER_CHANNEL
+$FLUTTER downgrade v2.2.1
+$FLUTTER doctor
 $FLUTTER config --enable-web
 $FLUTTER build web --web-renderer html --release
