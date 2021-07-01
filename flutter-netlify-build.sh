eaf42a179606b1d7322906b45bd88a42ca841554
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-07-01 21:06:40 +0700

    Update flutter version downgrade

--- flutter-netlify-build.sh
+++ flutter-netlify-build.sh
@@ -2,6 +2,5 @@ git clone https://github.com/flutter/flutter.git
 FLUTTER=flutter/bin/flutter
 FLUTTER_CHANNEL=stable
 $FLUTTER channel $FLUTTER_CHANNEL
-$FLUTTER upgrade
 $FLUTTER config --enable-web
 $FLUTTER build web --web-renderer html --release
