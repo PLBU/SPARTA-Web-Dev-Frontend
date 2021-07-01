bfaeff335caa5deb56e3267701edddc82d9641d5
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-07-01 12:26:40 +0700

    Changed flutter version to latest

--- flutter-netlify-build.sh
+++ flutter-netlify-build.sh
@@ -1,8 +1,7 @@
 git clone https://github.com/flutter/flutter.git
 FLUTTER=flutter/bin/flutter
 FLUTTER_CHANNEL=stable
-FLUTTER_VERSION=v2.2.1
 $FLUTTER channel $FLUTTER_CHANNEL
-$FLUTTER version $FLUTTER_VERSION
+$FLUTTER upgrade
 $FLUTTER config --enable-web
 $FLUTTER build web
