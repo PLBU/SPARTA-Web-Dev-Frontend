d30120a57bd88c96d8cfd1a4bb94c40e29eb00fe
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-07-07 12:48:52 +0700

    add namaPengirim to Support model

--- lib/models/support.dart
+++ lib/models/support.dart
@@ -2,15 +2,17 @@ class Support {
   final String id;
   final String penerima;
   final String pengirim;
+  final String namaPengirim;
   final String text;
 
-  const Support({this.id, this.penerima, this.pengirim, this.text});
+  const Support({this.id, this.penerima, this.pengirim, this.namaPengirim, this.text});
 
   factory Support.fromJson(dynamic json) {
     return Support(
       id: json['_id'] as String,
       penerima: json['penerima'] as String,
       pengirim: json['pengirim'] as String,
+      namaPengirim: json['namaPengirim'] as String,
       text: json['text'] as String,
     );
   }
